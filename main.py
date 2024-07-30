065d6bfbfc3d41232547fea7a2a20c488918f78b
A1lekse <alexey20080705@gmail.com>
2024-07-30 13:50:47 +0400

    init commut

---
+++ main.py
@@ -0,0 +1,6 @@
+from figures import circle_area, circle_perimeter
+from figures import square_perimeter, square_area
+from figures import triangle_perimeter, triangle_area
+
+print(circle_area(4))
+print(square_perimeter(4))
